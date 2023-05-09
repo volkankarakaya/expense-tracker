4587b36559e35ae52cd98c85c44f9d5181e73d1c
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-09 09:35:10 +0300

    Add Expense - Cancel Form buttons

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
 import './NewExpense.css'
-
+import { useState } from 'react'
 import ExpenseForm from './ExpenseForm'
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
+
     const saveExpenseDataHandler = (enteredExpenseData) =>{
         const expenseData = {
             ...enteredExpenseData,
@@ -12,10 +14,19 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData)
     }
 
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
         </div>
     )
 }
